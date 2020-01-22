5c9dd0eb77f4c85e29b2a61dd6fd97b43c92f63d
Blade Boles <bladerboles@gmail.com>
2020-01-22 15:34:03 -0700

    Finished validateBearerToken middleware

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ const movies = require('./movies');
 app.use(morgan('dev'));
 
 app.use(function validateBearerToken(req, res, next) {
+  
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+
   console.log('validate bearer token middleware');
-  debugger
+  
+  if(!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request'});
+  }
   next();
 });
 
